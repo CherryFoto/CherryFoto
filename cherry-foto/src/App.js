777174908d0dadf27ec9da0276c8df3645aa5e0a
crs <risheng17@gmail.com>
2020-01-18 13:10:21 +0800

    added dropzone

--- cherry-foto/src/App.js
+++ cherry-foto/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { DropzoneArea } from "material-ui-dropzone";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: []
+    };
+  }
+  handleChange(files) {
+    this.setState({
+      files: files
+    });
+  }
+  render() {
+    return <DropzoneArea onChange={this.handleChange.bind(this)} />;
+  }
 }
 
 export default App;
