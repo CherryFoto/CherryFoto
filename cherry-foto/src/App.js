ca7b2364695828b5a7aac843d26d6a03aeadac68
crs <risheng17@gmail.com>
2020-01-18 14:19:19 +0800

    small changes

--- cherry-foto/src/App.js
+++ cherry-foto/src/App.js
@@ -1,29 +1,11 @@
 import React, { Component } from "react";
-import { DropzoneArea } from "material-ui-dropzone";
+import UploadPhoto from "./UploadPhoto";
 import "./App.css";
-import { Button } from "@material-ui/core";
+import { Modal } from "@material-ui/core";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      files: []
-    };
-  }
-  handleChange(files) {
-    this.setState({
-      files: files
-    });
-  }
-  handleSubmit(files) {}
-
   render() {
-    return (
-      <div>
-        <DropzoneArea onChange={this.handleChange.bind(this)} />
-        <Button onClick={this.handleSubmit.bind(this)}>Filter</Button>
-      </div>
-    );
+    return <UploadPhoto />;
   }
 }
 
---
+++ cherry-foto/src/UploadPhoto.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { DropzoneArea } from "material-ui-dropzone";
+import "./App.css";
+import { Button, Divider } from "@material-ui/core";
+
+class UploadPhoto extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: []
+    };
+  }
+  handleChange(files) {
+    this.setState({
+      files: files
+    });
+  }
+  handleSubmit(files) {}
+
+  render() {
+    return (
+      <div>
+        <DropzoneArea onChange={this.handleChange.bind(this)} />
+        <Button onClick={this.handleSubmit.bind(this)}>Filter</Button>
+      </div>
+    );
+  }
+}
+
+export default UploadPhoto;
